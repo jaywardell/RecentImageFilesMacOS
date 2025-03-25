5f2c7fa491ab76395594ad978333de40ad25a594
Joseph Wardell <jay@jaywardell.me>
2025-03-25 11:20:16 -0400

    added dependencies for FileMenuActionsMacOS

--- Package.swift
+++ Package.swift
@@ -14,11 +14,17 @@ let package = Package(
             name: "RecentImageFilesMacOS",
             targets: ["RecentImageFilesMacOS"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/jaywardell/FileMenuActionsMacOS", .upToNextMajor(from: "0.1.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RecentImageFilesMacOS"),
+            name: "RecentImageFilesMacOS",
+            dependencies: [
+                "FileMenuActionsMacOS"
+            ]),
         .testTarget(
             name: "RecentImageFilesMacOSTests",
             dependencies: ["RecentImageFilesMacOS"]
